2e08d93b8153f30e6fb7d65078074da063699553
REgguria <145839362+REgguria@users.noreply.github.com>
2023-11-10 23:57:19 +0800

    Input Image and Refined User Header
    Placed a rough frame for user head and placed an image for the jumbotron
    the classes have not been specified <3

--- include/header.user.php
+++ include/header.user.php
@@ -1,2 +1,50 @@
-<header class = "sticky-top landing page">
-    <nav class= "navbar navbar-expand-lg navbar-dark bg-dark">
+<!-- Please place all proper classes THX -->
+<header class = "sticky-top">
+    <nav class = "navbar">
+        <div class= "container-fluid">
+            <a class="navbar-brand"href = "Index.php"><img src = "#" alt = ""></a>
+            <!-- Burger Button ID = navbarItems -->
+            <button class ="navbar-toggler" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarItems" 
+            aria-controls="navbarSupportedContent" aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <!-- End of Burger Button -->
+            <div class = "" id = "navbarItems">
+                <ul class=""> <!-- Navbar Item List Start -->
+                    <li class ="nav-link">
+                        <a href="Index.php">Home<a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="">Format</a>
+                        <!-- Navbar Item Name ^^^ -->
+                        <!-- Button for Format/ Format ID = FormatListItems -->
+                        <button class ="" type="button" data-bs-toggle="collapse" data-bs-target="#FormatListItems" 
+                        aria-controls ="" aria-expanded="false" aria-label="toggle navigation">
+                        <!-- Nested List for Format -->
+                        <ul class= "collapse" id ="FormatListItems">
+                            <li class = "">C++</li>
+                            <li class = "">Java</li>
+                            <li class = "">Phyton</li>
+                            <li class = "">CSS</li>
+                            <li class = "">PHP</li>
+                            <li class = "">JavaScript</li>
+                            <li class = "">More</li>
+                        </ul> <!-- End of Nested List -->
+                    </li>
+                    <li class = "nav-link">
+                        <a href="Index.php">News<a>
+                    </li>
+                    <li class = "nav-link">
+                        <a href="Index.php">Programmers<a>
+                    </li>
+                    <li class = "nav-link">
+                        <a href="Index.php">Sign Up<a>
+                    </li>
+                    <li class = "nav-link">
+                        <a href="Index.php">Log In<a>
+                    </li>
+                </ul> <!-- End of Navbar Item List -->
+            </div>
+        </div>
+    </nav>    
+</header>    
