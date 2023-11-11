2e7031b91f108fc7167b1734bb274cf338aef94e
REgguria <145839362+REgguria@users.noreply.github.com>
2023-11-11 08:46:45 +0800

    Created Header.admin.php
    Created a rough frame please expand on this later.

---
+++ include/header.admin.php
@@ -0,0 +1,36 @@
+<header class = "">
+    <div class = "">
+        <a class ="brand" href ="#">
+            <img src= "" alt="">
+        </a>
+        <!-- Sidebar burger button ID = sidebarMenu for siidebar.php -->
+        <button class= "navbar-toggler" type="button" data-bs-toggle="collapse" 
+        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" 
+        aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <nav class = "navbar navbar-expand-lg">
+            <div class = "container-fluid">
+                <div class ="navbar-collapse">
+                    <ul class = "navbar-nav me-auto">
+                        <li class ="nav-item dropdown">
+                            <a class ="nav-link dropdown-toggle" href = "#" id= "navbarDropdown" 
+                                role ="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="" class="rounded-circle me-1" 
+                                alt="User Image" width="48px" height="48px">
+                                     User Name
+                            </a>
+                            <ul class="dropdown-menu dropdown-profile" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="#">Profile</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Logout</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</header>    
