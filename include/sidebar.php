5792d812d3a398dfa920465e188f4c0efb1db36b
REgguria <145839362+REgguria@users.noreply.github.com>
2023-11-11 15:09:39 +0800

    Moved index.php to webpages folder
    added an empty sidebar.php for admin.

---
+++ include/sidebar.php
@@ -0,0 +1,54 @@
+<nav id="sidebarMenu" class="collapse">
+    <div class="">
+        <ul class="">
+            <li class ="nav-item">
+                <a class="nav-link> <?= $Dashboard_Page ?>" 
+                aria-current= "page" href ="dashboard.php">
+                    <!-- Idk what this does-->
+                    <i class = "fa fa-tachometer" aria="true"></i>
+                    Dashboard
+                </a>
+            </li>
+            <li class ="nav-item">
+                <a class="nav-link> <?= $For_Approval_Page?>" 
+                aria-hidden= "true" href ="#">
+                    <!-- Idk what this does-->
+                    <i class = "fa fa-tachometer" aria="true"></i>
+                    For Approval
+                </a>
+            </li>
+            <li class ="nav-item">
+                <a class="nav-link> <?= $Journals_Page?>" 
+                aria-hidden= "true" href ="#">
+                    <!-- Idk what this does-->
+                    <i class = "fa fa-tachometer" aria="true"></i>
+                    Journals
+                </a>
+            </li>
+            <li class ="nav-item">
+                <a class="nav-link> <?= $Reports_Page?>" 
+                aria-hidden= "true" href ="#">
+                    <!-- Idk what this does-->
+                    <i class = "fa fa-tachometer" aria="true"></i>
+                    Reports
+                </a>
+            </li>
+            <li class ="nav-item">
+                <a class="nav-link> <?= $Staff_Page?>" 
+                aria-hidden= "true" href ="#">
+                    <!-- Idk what this does-->
+                    <i class = "fa fa-tachometer" aria="true"></i>
+                    Staff
+                </a>
+            </li>
+            <li class ="nav-item">
+                <a class="nav-link> <?= $Settings_Page?>" 
+                aria-hidden= "true" href ="#">
+                    <!-- Idk what this does-->
+                    <i class = "fa fa-tachometer" aria="true"></i>
+                    Settings
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
