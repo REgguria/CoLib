3d9189a14e332acd73668f7f75d7c7de85268d15
Ralph <raaapido27@gmail.com>
2023-11-13 20:06:36 +0800

    Added additional containers and rows

--- Webpages/index.php
+++ Webpages/index.php
@@ -11,6 +11,17 @@
     <main>
         <div class="p-5 mb-4 bg-body-tertiary rounded-3">
             <div class="container-fluid py-5">
+
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="row">
+
+            </div>
+
+            <div class="row">
                 
             </div>
         </div>
