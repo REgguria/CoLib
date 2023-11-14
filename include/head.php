18f8c84e1a397b9c7f05534cc6432e8f06ee03b4
Ralph <raaapido27@gmail.com>
2023-11-15 02:36:45 +0800

    Added offline bootstrap

--- include/head.php
+++ include/head.php
@@ -5,6 +5,10 @@
     <!-- Bootstrap Online -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <!-- Bootstrap Offline -->
+    <link href="../vendor/bootstrap-5.0.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/dataTable-1.13.6/datatables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../vendor/font-awesome-4.7.0/css/font-awesome.min.css"/>
     <!-- add Custom CSS here -->
     <link rel="stylesheet" href="../CSS/Style.CSS">
 </head>
--- include/js.php
+++ include/js.php
@@ -3,3 +3,9 @@
 integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <!-- fontawsome js (if it fails to work download locally) -->
 <script src="https://kit.fontawesome.com/008d2189bb.js" crossorigin="anonymous"></script>
+<!-- Offline Version for Bootstrap -->
+<script src="../vendor/bootstrap-5.0.2/js/bootstrap.bundle.min.js"></script>
+<script src="../vendor/jquery-3.7.1/jquery.min.js"></script>
+<script src="../vendor/dataTable-1.13.6/datatables.min.js"></script>
+<script src="../vendor/pdfmake-0.2.7/pdfmake.min.js"></script>
+<script src="../vendor/pdfmake-0.2.7/vfs_fonts.js"></script>
