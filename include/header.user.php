c4863a9d450e729c8cd6040cecccce5bbde6dc9e
REgguria <145839362+REgguria@users.noreply.github.com>
2023-11-15 14:14:31 +0800

    need to fix responsiveness of navbar,
    changed navbar light --> dark to change text color

--- include/header.user.php
+++ include/header.user.php
@@ -1,19 +1,19 @@
 <!-- Please place all proper classes THX -->
 <header class = "sticky-top Landing-Page">
-    <nav class = "navbar navbar-expand-lg navbar-light bg-dark">
+    <nav class = "navbar navbar-expand-lg navbar-dark bg-dark">
         <div class= "container-fluid">
             <a class="navbar-brand" href = "Index.php"><img src = "../Images/Header/logo.png" alt = ""></a>
             <!-- Burger Button ID = navbarItems -->
             <button class ="navbar-toggler" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarItems" 
-            aria-controls="navbarSupportedContent" aria-expanded="false" 
+            aria-controls="navbarItems" aria-expanded="false" 
             aria-label="Toggle navigation">
-                <span class =navbar-toggler-icon>
-                </span>
+                <span class =navbar-toggler-icon> </span>
             </button>
             <!-- End of Burger Button -->
+            <!-- have to fix responsivenss-->
             <div class = "collapse navbar-collapse" id = "navbarItems">
-                <ul class="navbar-nav mb-2 mb-lg-o ms-auto me-0"> <!-- Navbar Item List Start -->
+                <ul class="navbar-nav mb-2 mb-lg-o ms-auto me-0"  > <!-- Navbar Item List Start -->
                     <li class ="nav-item">
                         <a class="nav-link" href="Index.php">Home<a>
                     </li>
