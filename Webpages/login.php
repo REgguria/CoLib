3a4343b55f8cf0d3f3edebdc078953b585f0f073
Ralph <raaapido27@gmail.com>
2023-11-24 01:33:48 +0800

    Added identifiers for variables/elements

--- Webpages/login.php
+++ Webpages/login.php
@@ -6,10 +6,10 @@
 ?>
 <body>
     <main>
-        <div class="container-fluid vh-100 d-flex justify-content-center align-items-center">
+        <div class="container-fluid vh-100 d-flex justify-content-center align-items-center" id="login-background">
             <div class="Login-Card rounded-3 border-1 border-primary p-4 m-5" style="width: 24rem;" id="Login-Card">
                 <p class="text-center">
-                    <img src="../img/logo1.png" class="img-fluid" alt="">
+                    <img src="../Images/Header/logo.png" class="img-fluid" alt="" id="login-logo">
                 </p>
                 <h1 class="h2 my-3 mb-4 text-center brand-color">Welcome to CoLib</h1>
                 <form method="post" action="">
@@ -21,7 +21,7 @@
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" name="password" required value="<?php if(isset($_POST['password'])) { echo $_POST['password']; } ?>">
                     </div>
-                    <button type="submit" class="btn btn-primary mt-2 brand-bg-color w-100" name="login">Login</button>
+                    <button type="submit" class="btn btn-primary mt-2 brand-bg-color w-100" name="login" id="login-btn">Login</button>
                 </form>
             </div> 
         </div>
