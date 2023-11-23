71c53d6c94f5b7f13723fc42a7b71536a6c3a848
Ralph <raaapido27@gmail.com>
2023-11-23 23:20:53 +0800

    initial commit

---
+++ Webpages/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        $title = "Login";
+        require_once('../include/head.php')
+    ?>
+
+</head>
+<body>
+    <main>
+        <div class="container-fluid w-100 border-3 d-flex justify-content-center align-items-center">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div> 
+        </div>
+    </main>
+    <?php 
+        require_once('../include/js.php');
+    ?>
+</body>
+</html>
