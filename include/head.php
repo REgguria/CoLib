1d2b8428000b93b1430c4addc6a7fc8388a6633f
Ralph <raaapido27@gmail.com>
2023-11-24 01:20:55 +0800

    Update head.php

--- include/head.php
+++ include/head.php
@@ -2,6 +2,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title><?php echo $title ?></title>
+    <!-- add Custom CSS here -->
+    <link rel="stylesheet" href="../CSS/Style.CSS">
     <!-- Bootstrap Online -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
@@ -9,6 +11,4 @@
     <link href="../vendor/bootstrap-5.0.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="../vendor/dataTable-1.13.6/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../vendor/font-awesome-4.7.0/css/font-awesome.min.css"/>
-    <!-- add Custom CSS here -->
-    <link rel="stylesheet" href="../CSS/Style.CSS">
 </head>
