8a5f1cbeda56be0ce2b5ad34671bf61c2e11fddb
BorGER <raaapido27@gmail.com>
2023-12-01 23:44:24 +0800

    things

--- include/head.php
+++ include/head.php
@@ -2,8 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo $title ?></title>
-    <!-- add Custom CSS here -->
-    <link rel="stylesheet" href="../CSS/Style.css">
     <!-- Bootstrap Online -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
@@ -11,4 +9,7 @@
     <link href="../vendor/bootstrap-5.0.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="../vendor/dataTable-1.13.6/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../vendor/font-awesome-4.7.0/css/font-awesome.min.css"/>
+    <!-- add Custom CSS here -->
+    <link rel="stylesheet" href="../CSS/Style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" charset="utf+8"></script>
 </head>
