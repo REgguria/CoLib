b9aeb9581a32ab80e0b66f6290e863308adaa686
Ralph <raaapido27@gmail.com>
2023-12-02 20:46:54 +0800

    Fixed Story

--- Webpages/aboutus.php
+++ Webpages/aboutus.php
@@ -13,15 +13,15 @@
     <main class="container-fluid bg-secondary vh-100">
         <div class="container-fluid">
             <div class="container-fluid d-flex justify-content-center align-items-center">
-                <div class="card m-5" style="max-width: 24em ">
-                    <img src="../Images/kamilphoto.jpg" class="card-img-top img-fluid"/>
+                <div class="card m-5" style="width: 24em">
+                    <img src="../Images/kamilphoto.jpg" class="card-img-top img-fluid" style="max-height: 20em;"/>
                     <div class="card-body">
                         <h5 class="card-title">Kamil Abdurajie</h5>
                         <p class="card-text">Founder of CoLib</p>
                     </div>
                 </div>
-                <div class="card m-5" style="max-width: 24em">
-                    <img src="../Images/AAAAAAAA.png" class="card-img-top img-fluid"/>
+                <div class="card m-5" style="width: 24em">
+                    <img src="../Images/AAAAAAAA.png" class="card-img-top img-fluid" style="max-height: 20em;"/>
                     <div class="card-body">
                         <h5 class="card-title">Ralph Waldo B. Lahaman</h5>
                         <p class="card-text">Co-Founder of Colib</p>
